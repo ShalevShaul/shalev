de37c948d5202cd10b6de3e6a4744c47554f1971
Shalev Shaul <shalevshaul1@gmail.com>
2026-05-28 11:54:36 +0300

    feat(layout): mount CookieBanner inside NextIntlClientProvider

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import { getMessages } from 'next-intl/server'
 import { ThemeProvider } from 'next-themes'
 import { Analytics } from '@vercel/analytics/next'
 import { GoogleAnalytics } from '@next/third-parties/google'
+import CookieBanner from '@/components/ui/CookieBanner'
 import { routing } from '@/i18n/routing'
 import '../globals.css'
 
@@ -223,6 +224,7 @@ export default async function LocaleLayout({ children, params }: Props) {
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           <NextIntlClientProvider messages={messages}>
             {children}
+            <CookieBanner />
           </NextIntlClientProvider>
           <Analytics />
         </ThemeProvider>
